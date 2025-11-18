3c27fda7a6a604d36b08707ea565a5600aa03812
superheroes6 <santibox888@gmail.com>
2025-11-18 12:31:17 +0100

    commit 7

--- src/POO_9/AccesModifiers.java
+++ src/POO_9/AccesModifiers.java
@@ -1,5 +1,14 @@
 package POO_9;
 
 public class AccesModifiers {
-
+	public static void main(String[] args) {
+		
+		/*Encapsulamiento (modificadores de acceso)
+		 * - public = se accede desde cualquier parte del programa
+		 * - private = solo desde la clase
+		 * - protected = desde la misma calse y subclases
+		 * - default = solo en el mismo paquete
+		 */
+		
+	}
 }
